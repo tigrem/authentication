a1461f9991ff73e5635bf4eb2e683c3ae675e668
Tigrem Sahilu <106732905+tigrem@users.noreply.github.com>
2025-11-25 13:04:27 +0300

    Update and rename pri.ts to prisma.config.ts

--- prisma.config.ts
+++ prisma.config.ts
@@ -1,11 +1,10 @@
-// prisma.config.ts (in your project root)
+// prisma.config.ts
 
 import { defineConfig } from "@prisma/config";
 
 export default defineConfig({
   datasources: {
     db: {
-      // Reference the environment variable here for the CLI tools (generate/migrate)
       url: process.env.DATABASE_URL,
     },
   },
