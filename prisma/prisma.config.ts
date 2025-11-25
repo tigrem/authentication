90c9e0563442f2eb93bbe9b1132c800e21cb8636
Tigrem Sahilu <106732905+tigrem@users.noreply.github.com>
2025-11-25 09:39:27 +0300

    Update prisma.config.ts

--- prisma/prisma.config.ts
+++ prisma/prisma.config.ts
@@ -1,11 +1,1 @@
-// prisma.config.ts (in your project root)
 
-import { defineConfig } from "@prisma/config";
-
-export default defineConfig({
-  datasources: {
-    db: {
-      url: process.env.DATABASE_URL,
-    },
-  },
-});
